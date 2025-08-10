0a60d8e4bc96c53b9cc91ecacd3d181d48bb09fc
MarioTawadrous <mario.tawadrous.95@gmail.com>
2025-08-11 00:21:04 +0300

    components/commons/Button.tsx, components/layout/Header.tsx,components/layout/Footer.tsx, components/layout/Layout.tsx, pages/_app.tsx, pages/index.tsx

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,10 +1,13 @@
-import { ComponentProps } from "react";
+import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ComponentProps> = ({ children }) => {
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-    <>
-      <div>Button Page</div>
-    </>
+    <button
+      onClick={action}
+      className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300"
+    >
+      {title}
+    </button>
   );
 };
 
