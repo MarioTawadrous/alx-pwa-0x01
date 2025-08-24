bd736c1a13791f8f294eb5860400e104e53b2d07
MarioTawadrous <mario.tawadrous.95@gmail.com>
2025-08-24 22:54:48 +0300

    Create a Web App Manifest

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,12 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
-
-export default nextConfig;
-
 import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
